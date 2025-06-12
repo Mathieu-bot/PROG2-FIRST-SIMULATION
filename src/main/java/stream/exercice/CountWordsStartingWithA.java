9edecd66da1a58e41511f2ffec614957dc9a3d83
Mathieu <hei.tafita.2@gmail.com>
2025-06-12 13:27:16 +0300

    feat: update method

--- src/main/java/stream/exercice/CountWordsStartingWithA.java
+++ src/main/java/stream/exercice/CountWordsStartingWithA.java
@@ -1,4 +1,9 @@
 package stream.exercice;
 
+import java.util.List;
+
 public class CountWordsStartingWithA {
+  public static long countWordsStartWithA (List<String> str) {
+    return str.stream().filter(e -> e.startsWith("A")).count();
+  }
 }
