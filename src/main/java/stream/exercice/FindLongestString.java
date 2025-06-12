ea71d96c47f20467296304280d57e6a3527088d6
Mathieu <hei.tafita.2@gmail.com>
2025-06-12 13:40:49 +0300

    chore: clean code

--- src/main/java/stream/exercice/FindLongestString.java
+++ src/main/java/stream/exercice/FindLongestString.java
@@ -4,11 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class FindLongestString {
-  public static Optional<String> findLongestString (List<String> strings) {
+  public static Optional<String> findLongestString(List<String> strings) {
     return strings.stream().reduce("", (a, v) -> v.length() > a.length() ? a = v : a).describeConstable();
-  }
-
-  public static void main(String[] args) {
-    System.out.println(t(List.of("bon", "jour")));
   }
 }
