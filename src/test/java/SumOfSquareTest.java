313343545684ba093a984fb1e6f679dd13435fc3
Mathieu <hei.tafita.2@gmail.com>
2025-06-12 13:25:35 +0300

    feat: add Test directory

---
+++ src/test/java/SumOfSquareTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SumOfSquareTest {
+//
+//  @Test
+//  @DisplayName("A method who return the sum of square of each value in a list")
+//  void sumOfSquare() {
+//    assertAll(
+//      assertEquals();
+//    );
+//  }
+}
