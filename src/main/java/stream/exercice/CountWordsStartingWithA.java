54b2c1afc7f6cffed9396009b89456922434a49a
mayahNeko <mayahibela@gmail.com>
2025-06-12 13:18:03 +0300

    add changes

--- src/main/java/stream/exercice/CountWordsStartingWithA.java
+++ src/main/java/stream/exercice/CountWordsStartingWithA.java
@@ -1,4 +1,9 @@
 package stream.exercice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CountWordsStartingWithA {
+    public static long CountWordsStartingWithA(List<String> words)
+    }
 }
