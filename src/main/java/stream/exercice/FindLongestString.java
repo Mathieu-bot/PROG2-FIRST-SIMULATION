44bd94bde61218c0cb7946854b7a7e9c331d18bb
Mathieu <hei.tafita.2@gmail.com>
2025-06-12 13:25:35 +0300

    feat: add FindLongestString class

---
+++ src/main/java/stream/exercice/FindLongestString.java
@@ -0,0 +1,14 @@
+package stream.exercice;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FindLongestString {
+  public static Optional<String> findLongestString (List<String> strings) {
+    return strings.stream().reduce("", (a, v) -> v.length() > a.length() ? a = v : a).describeConstable();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(t(List.of("bon", "jour")));
+  }
+}
