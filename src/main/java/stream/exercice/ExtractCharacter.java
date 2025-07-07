a6f164a40ca9bd5e53b11274ee53423d8fa04a91
Mathieu <hei.tafita.2@gmail.com>
2025-07-07 18:03:28 +0300

    Feat: add ExtractCharacter class to extract characters from a list of strings

---
+++ src/main/java/stream/exercice/ExtractCharacter.java
@@ -0,0 +1,14 @@
+package stream.exercice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExtractCharacter {
+//  public static List<Character> extractCharacters(List<String> words) {
+////    return words.stream().flatMap()
+//  }
+
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+}
